c6eda90717e19269d30d25b0dc2dad0627ba6534
David Mauriello <dmauriello82@gmail.com>
2021-02-08 17:20:34 -0500

    Longest increment assignment

--- src/longestIncrement/LongestIncrementMain.java
+++ src/longestIncrement/LongestIncrementMain.java
@@ -8,10 +8,15 @@ public class LongestIncrementMain {
 	public static void main(String[] args) {
 		int n = 100;
 		SequenceGenerator sg = new SequenceGenerator(n);
-		ArrayList<Integer> sequence = sg.GenerateSequence();
+		int[] sequence = sg.generateSequence();
 		
 		for (int i: sequence) {
 			System.out.print(i + " ");
 		}
+		System.out.print("\n");
+		
+		ArrayList<Integer> longest = sg.longestSubsequence();
+		System.out.println("Longest Subsequence: " + longest.toString());
 	}
+	
 }
--- src/longestIncrement/SequenceGenerator.java
+++ src/longestIncrement/SequenceGenerator.java
@@ -5,11 +5,11 @@ import java.util.ArrayList;
 public class SequenceGenerator {
 	
 	private int n;
-	private ArrayList<Integer> array;
+	private int[] array;
 	
 	public SequenceGenerator(int n) {
 		this.n = n;
-		array = new ArrayList<Integer>();
+		array = new int[100];
 	}
 	
 	public int generateInt() {
@@ -19,13 +19,37 @@ public class SequenceGenerator {
 		return randomInt;
 	}
 
-	public ArrayList<Integer> GenerateSequence(){
-		array.clear();
+	public int[] generateSequence(){
 		
 		for (int i = 0; i < n; i++) {
-			array.add(this.generateInt());
+			array[i] = this.generateInt();
 		}
 		
 		return array;
+	}
+	
+	public ArrayList<Integer> longestSubsequence(){
+		ArrayList<Integer> longest = new ArrayList<Integer>();
+		ArrayList<Integer> temp = new ArrayList<Integer>();
+		
+		for (int i: array) {
+			if (temp.size() == 0) {
+				temp.add(i);
+				continue;
+			}
+			if (temp.get(temp.size()-1) >= i) {
+				//System.out.println(i + " is less than " + temp.get(temp.size()-1));
+				if (temp.size() > longest.size()) {
+					longest.clear();
+					for(int j: temp) {
+						longest.add(j);
+					}
+					//System.out.println("longest array is now: " + longest);
+				}
+				temp.clear();
+			}
+			temp.add(i);	
+		}
+		return longest;
 	}
 }
